9e8c0dc03ef9975c4df1aa6eaab76467fec51396
nagatomo <reiji128@icloud.com>
2021-08-12 17:42:00 +0900

    20210812　カメラとプレイヤーの位置調整

--- World Swim Game/Title.cpp
+++ World Swim Game/Title.cpp
@@ -1,156 +1,156 @@
-//#include "Title.h"
-//#include "GameScene.h"
-//
-//#include "DxLib.h"
-//
-//const int SCREEN_SIZE_W = 1920;
-//const int SCREEN_SIZE_H = 1080;
-//
-//const int addAlphaVal = 5;
-//
-//Title::Title()
-//	: m_state(TITLE_SCENE_STATE::TITLE)
-//	, m_alphaVal(0)
-//	, m_fadeOutFinishFlag(false)
-//{
-//	if (CheckHitKey(KEY_INPUT_RETURN))
-//	{
-//		m_checkKeyFlag = TRUE;
-//	}
-//
-//	SetFontSize(m_normalFontSize);
-//}
-//
-//Title::~Title()
-//{
-//	StopSoundMem(m_soundHandle);
-//
-//	DeleteGraph(m_backGraphHandle);
-//	DeleteSoundMem(m_soundHandle);
-//}
-//
-//SceneBase* Title::Update(float _deltaTime)
-//{
-//	// ステートメントごとに処理を変更
-//	switch (m_state)
-//	{
-//	case TITLE_SCENE_STATE::FADE_IN:
-//		break;
-//	case TITLE_SCENE_STATE::TITLE:
-//		// ※キー入力重複対策のフラグ
-//		// ENTERキーから指を離したら、次のENTERの入力を有効に
-//		if (!CheckHitKey(KEY_INPUT_RETURN))
-//		{
-//			m_checkKeyFlag = false;
-//		}
-//
-//		// ENTERで次のステートへ
-//		if (CheckHitKey(KEY_INPUT_RETURN) && m_checkKeyFlag == false)
-//		{
-//			// ※キー入力重複対策のフラグ
-//			m_checkKeyFlag = true;
-//
-//			m_state = TITLE_SCENE_STATE::FADE_OUT;
-//		}
-//
-//		break;
-//	case TITLE_SCENE_STATE::MANUAL:
-//		if (!CheckHitKey(KEY_INPUT_RETURN))
-//		{
-//			m_checkKeyFlag = false;
-//		}
-//
-//		if (CheckHitKey(KEY_INPUT_RETURN) && m_checkKeyFlag == false)
-//		{
-//			// ※キー入力重複対策のフラグ
-//			m_checkKeyFlag = true;
-//
-//			m_state = TITLE_SCENE_STATE::TITLE;
-//		}
-//
-//		break;
-//	case TITLE_SCENE_STATE::FADE_OUT:
-//		if (m_fadeOutFinishFlag)
-//		{
-//			return new GameScene();
-//		}
-//		break;
-//	default:
-//		break;
-//	}
-//	return this;
-//}
-//
-//void Title::Draw()
-//{
-//	// 背景を描画
-//	DrawGraph(0, 0, m_backGraphHandle, TRUE);
-//
-//	if (m_state == TITLE_SCENE_STATE::MANUAL)
-//	{
-//		// 透過して描画
-//		SetDrawBlendMode(DX_BLENDMODE_ALPHA, 190);
-//		DrawBox(0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H, GetColor(0, 0, 0), TRUE);
-//		// 透過を元に戻す
-//		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
-//	}
-//
-//	//	フェードイン処理
-//	if (m_state == TITLE_SCENE_STATE::FADE_IN)
-//	{
-//		// アルファ値の減算
-//		m_alphaVal -= addAlphaVal;
-//
-//		// アルファブレンド有効化(ここでアルファ値をセット)
-//		SetDrawBlendMode(DX_BLENDMODE_ALPHA, m_alphaVal);
-//
-//		// 画面全体に任意のカラーの四角形を描画
-//		DrawBox(0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H, GetColor(0, 0, 0), TRUE);
-//
-//		// アルファブレンド無効化
-//		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
-//
-//		// アルファ値が最大(255)になったらフェードアウト終了
-//		if (m_alphaVal <= 0)
-//		{
-//			m_state = TITLE_SCENE_STATE::TITLE;
-//		}
-//	}
-//
-//	// フェードアウト処理
-//	if (m_state == TITLE_SCENE_STATE::FADE_OUT)
-//	{
-//		// アルファ値の加算
-//		m_alphaVal += addAlphaVal;
-//		// アルファブレンド有効化(ここでアルファ値をセット)
-//		SetDrawBlendMode(DX_BLENDMODE_ALPHA, m_alphaVal);
-//
-//		// 画面全体に任意のカラーの四角形を描画
-//		DrawBox(0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H, GetColor(0, 0, 0), TRUE);
-//
-//		// アルファブレンド無効化
-//		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
-//
-//		// アルファ値が最大(255)になったらフェードアウト終了
-//		if (m_alphaVal >= 255)
-//		{
-//			m_fadeOutFinishFlag = true;
-//		}
-//	}
-//}
-//
-//void Title::Sound()
-//{
-//	//	BGMを流す
-//	PlaySoundMem(m_soundHandle, DX_PLAYTYPE_BACK, FALSE);
-//	ChangeVolumeSoundMem(m_volumePal, m_soundHandle);
-//}
-//
-//void Title::Load()
-//{
-//	//	グラフィックハンドルにセット
-//	m_backGraphHandle = LoadGraph("data/img/titleBackTest.png");			//	背景
-//
-//	//	サウンドハンドルにセット
-//	m_soundHandle = LoadSoundMem("data/sound/titleBgmTest.mp3");			//	BGM
-//}
+#include "Title.h"
+#include "GameScene.h"
+
+#include "DxLib.h"
+
+const int SCREEN_SIZE_W = 1920;
+const int SCREEN_SIZE_H = 1080;
+
+const int addAlphaVal = 5;
+
+Title::Title()
+	: m_state(TITLE_SCENE_STATE::TITLE)
+	, m_alphaVal(0)
+	, m_fadeOutFinishFlag(false)
+{
+	if (CheckHitKey(KEY_INPUT_RETURN))
+	{
+		m_checkKeyFlag = TRUE;
+	}
+
+	SetFontSize(m_normalFontSize);
+}
+
+Title::~Title()
+{
+	StopSoundMem(m_soundHandle);
+
+	DeleteGraph(m_backGraphHandle);
+	DeleteSoundMem(m_soundHandle);
+}
+
+SceneBase* Title::Update(float _deltaTime)
+{
+	// ステートメントごとに処理を変更
+	switch (m_state)
+	{
+	case TITLE_SCENE_STATE::FADE_IN:
+		break;
+	case TITLE_SCENE_STATE::TITLE:
+		// ※キー入力重複対策のフラグ
+		// ENTERキーから指を離したら、次のENTERの入力を有効に
+		if (!CheckHitKey(KEY_INPUT_RETURN))
+		{
+			m_checkKeyFlag = false;
+		}
+
+		// ENTERで次のステートへ
+		if (CheckHitKey(KEY_INPUT_RETURN) && m_checkKeyFlag == false)
+		{
+			// ※キー入力重複対策のフラグ
+			m_checkKeyFlag = true;
+
+			m_state = TITLE_SCENE_STATE::FADE_OUT;
+		}
+
+		break;
+	case TITLE_SCENE_STATE::MANUAL:
+		if (!CheckHitKey(KEY_INPUT_RETURN))
+		{
+			m_checkKeyFlag = false;
+		}
+
+		if (CheckHitKey(KEY_INPUT_RETURN) && m_checkKeyFlag == false)
+		{
+			// ※キー入力重複対策のフラグ
+			m_checkKeyFlag = true;
+
+			m_state = TITLE_SCENE_STATE::TITLE;
+		}
+
+		break;
+	case TITLE_SCENE_STATE::FADE_OUT:
+		if (m_fadeOutFinishFlag)
+		{
+			return new GameScene();
+		}
+		break;
+	default:
+		break;
+	}
+	return this;
+}
+
+void Title::Draw()
+{
+	// 背景を描画
+	DrawGraph(0, 0, m_backGraphHandle, TRUE);
+
+	if (m_state == TITLE_SCENE_STATE::MANUAL)
+	{
+		// 透過して描画
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, 190);
+		DrawBox(0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H, GetColor(0, 0, 0), TRUE);
+		// 透過を元に戻す
+		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+	}
+
+	//	フェードイン処理
+	if (m_state == TITLE_SCENE_STATE::FADE_IN)
+	{
+		// アルファ値の減算
+		m_alphaVal -= addAlphaVal;
+
+		// アルファブレンド有効化(ここでアルファ値をセット)
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, m_alphaVal);
+
+		// 画面全体に任意のカラーの四角形を描画
+		DrawBox(0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H, GetColor(0, 0, 0), TRUE);
+
+		// アルファブレンド無効化
+		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+
+		// アルファ値が最大(255)になったらフェードアウト終了
+		if (m_alphaVal <= 0)
+		{
+			m_state = TITLE_SCENE_STATE::TITLE;
+		}
+	}
+
+	// フェードアウト処理
+	if (m_state == TITLE_SCENE_STATE::FADE_OUT)
+	{
+		// アルファ値の加算
+		m_alphaVal += addAlphaVal;
+		// アルファブレンド有効化(ここでアルファ値をセット)
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, m_alphaVal);
+
+		// 画面全体に任意のカラーの四角形を描画
+		DrawBox(0, 0, SCREEN_SIZE_W, SCREEN_SIZE_H, GetColor(0, 0, 0), TRUE);
+
+		// アルファブレンド無効化
+		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+
+		// アルファ値が最大(255)になったらフェードアウト終了
+		if (m_alphaVal >= 255)
+		{
+			m_fadeOutFinishFlag = true;
+		}
+	}
+}
+
+void Title::Sound()
+{
+	//	BGMを流す
+	PlaySoundMem(m_soundHandle, DX_PLAYTYPE_BACK, FALSE);
+	ChangeVolumeSoundMem(m_volumePal, m_soundHandle);
+}
+
+void Title::Load()
+{
+	//	グラフィックハンドルにセット
+	m_backGraphHandle = LoadGraph("data/img/titleBackTest.png");			//	背景
+
+	//	サウンドハンドルにセット
+	m_soundHandle = LoadSoundMem("data/sound/titleBgmTest.mp3");			//	BGM
+}
